af7625e2f7a49084723e36b3e8022eda55372cf7
Юля Кобзева <selemina@mail.ru>
2022-09-02 00:14:16 +0300

    ДЗ 2

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String authorName;
     private String authorSurname;
@@ -15,4 +17,21 @@ public class Author {
         return this.authorSurname;
     }
 
+    @Override
+    public String toString() {
+        return this.authorName + this.authorSurname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return authorName.equals(author.authorName) && authorSurname.equals(author.authorSurname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authorName, authorSurname);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        Author chehov = new Author("Антон", "Чехов");
-        Author rowling = new Author("Джоан", "Роулинг");
-        Author remark = new Author("Эрих Мария", "Ремарк");
+        Author chehov = new Author("Антон ", "Чехов ");
+        Author rowling = new Author("Джоан ", "Роулинг ");
+        Author remark = new Author("Эрих Мария ", "Ремарк ");
 
-        Book stories = new Book("Рассказы", chehov, 1985);
-        Book potter = new Book("Гарри Поттер", rowling, 2005);
-        Book three_comrades = new Book("Три товарища", remark, 2015);
+        Book stories = new Book("Рассказы ", chehov, 1985);
+        Book potter = new Book("Гарри Поттер ", rowling, 2005);
+        Book three_comrades = new Book("Три товарища ", remark, 2015);
 
         potter.setYear(2022);
 
         System.out.println(potter.getName() + " " + rowling.getAuthorName() + " " + rowling.getAuthorSurname() + " " + potter.getYear());
+
+        Book [] books = new Book[3];
+        books[0] = stories;
+        books[1] = potter;
+        books[2] = three_comrades;
+        for (int i = 0; i < books.length; i++){
+            System.out.println(books[i].getName() + ": " + books[i].getName2() + ": " + books[i].getYear());
+        }
+
+        System.out.println(three_comrades);
     }
 }
